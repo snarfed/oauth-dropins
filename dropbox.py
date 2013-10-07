50656f18b7f7cb7ba9094d01ee61b622f06d4b80
Ryan Barrett <git@ryanb.org>
2013-10-07 07:54:31 -0700

    first pass at dropbox

---
+++ dropbox.py
@@ -0,0 +1,112 @@
+"""Dropbox OAuth drop-in.
+
+https://www.dropbox.com/developers/core/start/python
+https://www.dropbox.com/static/developers/dropbox-python-sdk-1.6-docs/
+https://www.dropbox.com/developers/core/docs
+"""
+
+import json
+import logging
+import urllib
+
+import appengine_config
+from python_dropbox.client import DropboxOAuth2Flow, DropboxClient
+import models
+from webob import exc
+from webutil import handlers
+from webutil import util
+
+from google.appengine.ext import db
+import models
+import webapp2
+
+
+OAUTH_CALLBACK = '%s/dropbox/oauth_callback'
+CSRF_PARAM = 'dropbox-auth-csrf-token'
+
+
+class DropboxCsrf(db.Model):
+  """Stores a CSRF token for the Dropbox OAuth2 flow."""
+  token = db.StringProperty(required=False)
+
+
+class Dropbox(db.Model):
+  """A Dropbox account. The key name is the user id."""
+  access_token = db.StringProperty(required=True)
+
+
+def handle_exception(self, e, debug):
+  """Exception handler that handles Dropbox client errors.
+  """
+  if isinstance(e, (DropboxOAuth2Flow.CsrfException,
+                      DropboxOAuth2Flow.ProviderException)):
+    logging.exception()
+    raise exc.HTTPForbidden()
+  elif isinstance(e, (DropboxOAuth2Flow.BadRequestException,
+                      DropboxOAuth2Flow.BadStateException,
+                      DropboxOAuth2Flow.NotApprovedException)):
+    logging.exception()
+    raise exc.HTTPBadRequest()
+  else:
+    return handlers.handle_exception(self, e, debug)
+
+
+class StartHandler(webapp2.RequestHandler):
+  """Starts Dropbox auth. Requests an auth code and expects a redirect back.
+  """
+  handle_exception = handle_exception
+
+  def post(self):
+    csrf = DropboxCsrf()
+    csrf.save()
+    csrf_holder = {}
+    flow = DropboxOAuth2Flow(appengine_config.DROPBOX_APP_KEY,
+                             appengine_config.DROPBOX_APP_SECRET,
+                             OAUTH_CALLBACK % self.request.host_url,
+                             csrf_holder, CSRF_PARAM)
+
+    auth_url = flow.start(url_state=str(csrf.key().id()))
+
+    csrf.token = csrf_holder[CSRF_PARAM]
+    csrf.save()
+    logging.info('Stored DropboxCsrf id %d, redirecting to Dropbox: %s',
+      csrf.key().id(), auth_url)
+    self.redirect(auth_url)
+
+
+class CallbackHandler(webapp2.RequestHandler):
+  """The auth callback. Fetches an access token, stores it, and redirects home.
+  """
+  handle_exception = handle_exception
+
+  def get(self):
+    # lookup the CSRF token
+    csrf_id = self.request.get('state').split('|')[1]
+    csrf = DropboxCsrf.get_by_id(int(csrf_id))
+    if not csrf:
+      raise exc.HTTPBadRequest('No CSRF token for id %s', csrf_id)
+
+    # extract the OAuth access token
+    csrf_holder = {CSRF_PARAM: csrf.token}
+    flow = DropboxOAuth2Flow(appengine_config.DROPBOX_APP_KEY,
+                             appengine_config.DROPBOX_APP_SECRET,
+                             OAUTH_CALLBACK % self.request.host_url,
+                             csrf_holder, CSRF_PARAM)
+    access_token, user_id, state = flow.finish(self.request.params)
+
+    logging.info('Storing new Dropbox account: %s', user_id)
+    dropbox = Dropbox.get_or_insert(key_name=user_id,
+                                    access_token=access_token)
+
+    # redirect so that refreshing the page doesn't try to regenerate the oauth
+    # token, which won't work.
+    self.redirect('/?%s' % urllib.urlencode(
+        {'dropbox_id': user_id,
+         'dropbox_access_token': util.ellipsize(access_token),
+         }))
+
+
+application = webapp2.WSGIApplication([
+    ('/dropbox/start', StartHandler),
+    ('/dropbox/oauth_callback', CallbackHandler),
+    ], debug=appengine_config.DEBUG)
