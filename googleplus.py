fa66f89867b25ba519e4ed2dd85731938e7d93f6
Ryan Barrett <git@ryanb.org>
2013-10-13 08:32:38 -0700

    implement BaseAuth API for Google+

--- googleplus.py
+++ googleplus.py
@@ -1,4 +1,7 @@
 """Google+ OAuth drop-in.
+
+Google+ API docs: https://developers.google.com/+/api/latest/
+Python API client docs: https://developers.google.com/api-client-library/python/
 """
 
 import json
@@ -14,16 +17,15 @@ from webutil import util
 
 from apiclient import discovery
 from apiclient.errors import HttpError
-from oauth2client.appengine import CredentialsModel
-from oauth2client.appengine import OAuth2Decorator
-from oauth2client.appengine import StorageByKeyName
+from oauth2client.appengine import CredentialsModel, OAuth2Decorator, StorageByKeyName
+from oauth2client.client import Credentials, OAuth2Credentials
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 import webapp2
 
 
 # service names and versions:
-# https://developers.google.com/api-client-library/python/reference/supported_apis
+# https://developers.google.com/api-client-library/python/apis/
 json_service = discovery.build('plus', 'v1')
 oauth = OAuth2Decorator(
   client_id=appengine_config.GOOGLE_CLIENT_ID,
@@ -33,16 +35,57 @@ oauth = OAuth2Decorator(
   callback_path='/googleplus/oauth2callback')
 
 
-# TODO: port to models.Site
-class GooglePlusAuth(db.Model):
-  """A Google+ account. The key name is the Google+ user id."""
-  info_json = db.TextProperty(required=True)
+class GooglePlusAuth(models.BaseAuth):
+  """An authenticated Google+ user or page.
+
+  Provides methods that return information about this user (or page) and make
+  OAuth-signed requests to the Google+ API. Stores OAuth credentials in the
+  datastore. See models.BaseAuth for usage details.
+
+  Google+-specific details: implements http() and api() but not urlopen(). api()
+  returns a apiclient.discovery.Resource. The datastore entity key name is the
+  Google+ user id.
+  """
+  user_json = db.TextProperty(required=True)
   creds_json = db.TextProperty(required=True)
+
+  def site_name(self):
+    return 'Google+'
+
+  def user_display_name(self):
+    """Returns the user's name.
+    """
+    return json.loads(user_json)['displayName']
+
+  def creds(self):
+    """Returns an oauth2client.OAuth2Credentials.
+    """
+    return OAuth2Credentials.from_json(self.creds_json)
+
+  def http(self):
+    """Returns an httplib2.Http that adds OAuth credentials to requests.
+    """
+    http = httplib2.Http()
+    self.creds().authorize(http)
+    return http
+
+  def api(self):
+    """Returns an apiclient.discovery.Resource for the Google+ JSON API.
+
+    To use it, first choose a resource type (e.g. People), then make a call,
+    then execute that call with an authorized Http instance. For example:
+
+    gpa = GooglePlusAuth.get_by_key_name(123)
+    results_json = gpa.people().search(query='ryan').execute(gpa.http())
+
+    More details: https://developers.google.com/api-client-library/python/
+    """
+    return json_service
 
 
 class StartHandler(webapp2.RequestHandler):
-  """Adds a Google+ account. Authenticates via OAuth if necessary."""
-
+  """Finishes Facebook auth. (The oauth decorator handles OAuth redirects.)
+  """
   def handle_exception(self, e, debug):
     """Exception handler that passes back HttpErrors as real HTTP errors.
     """
@@ -59,8 +102,9 @@ class StartHandler(webapp2.RequestHandler):
     user = json_service.people().get(userId='me').execute(oauth.http())
     logging.debug('Got one person: %r', user)
     creds_json = oauth.credentials.to_json()
-    GooglePlusAuth.get_or_insert(key_name=user['id'], creds_json=creds_json,
-                                 info_json=json.dumps(user))
+    GooglePlusAuth.get_or_insert(key_name=user['id'],
+                                 creds_json=creds_json,
+                                 user_json=json.dumps(user))
 
     # redirect so that refreshing doesn't rewrite this GooglePlus entity
     self.redirect('/?%s' % urllib.urlencode(
