63d9cc8cbf33cfa9a6ad4d6338c6fababf515572
Ryan Barrett <git@ryanb.org>
2013-12-28 18:33:54 -0800

    TwitterAuth: pull out auth_header() method, promote _auth() to tweepy_auth()

--- twitter.py
+++ twitter.py
@@ -68,29 +68,43 @@ class TwitterAuth(models.BaseAuth):
                                       **kwargs)
 
   @staticmethod
-  def signed_urlopen(url, token_key, token_secret, **kwargs):
-    """Wraps urllib2.urlopen() and adds an OAuth signature.
+  def auth_header(url, token_key, token_secret):
+    """Generates an Authorization header and returns it in a header dict.
+
+    Args:
+      url: string
+      token_key: string
+      token_secret: string
+
+    Returns: single element dict with key 'Authorization'
     """
     parsed = urlparse.urlparse(url)
     url_without_query = urlparse.urlunparse(list(parsed[0:4]) + ['', ''])
-    headers = {}
-    auth = TwitterAuth._auth(token_key, token_secret)
-    auth.apply_auth(url_without_query, 'GET', headers,
+    header = {}
+    auth = TwitterAuth.tweepy_auth(token_key, token_secret)
+    auth.apply_auth(url_without_query, 'GET', header,
                     dict(urlparse.parse_qsl(parsed.query)))
     logging.debug(
-      'Populated Authorization header from access token key %s... and secret %s...',
+      'Generated Authorization header from access token key %s... and secret %s...',
       token_key[:4], token_secret[:4])
-      # headers.get('Authorization'))
-    logging.debug('Fetching %s', url)
-    return urllib2.urlopen(urllib2.Request(url, headers=headers), **kwargs)
-
-  def _api(self):
-    """Returns a tweepy.API.
-    """
-    return tweepy.API(TwitterAuth._auth(self.token_key, self.token_secret))
+      # header.get('Authorization'))
+    return header
 
   @staticmethod
-  def _auth(token_key, token_secret):
+  def signed_urlopen(url, token_key, token_secret, **kwargs):
+    """Wraps urllib2.urlopen() and adds an OAuth signature.
+    """
+    header = TwitterAuth.auth_header(url, token_key, token_secret)
+    logging.debug('Fetching %s', url)
+    return urllib2.urlopen(urllib2.Request(url, headers=header), **kwargs)
+
+  def tweepy_api(self):
+    """Returns a tweepy.API.
+    """
+    return tweepy.API(TwitterAuth.tweepy_auth(self.token_key, self.token_secret))
+
+  @staticmethod
+  def tweepy_auth(token_key, token_secret):
     """Returns a tweepy.OAuthHandler.
     """
     auth = tweepy.OAuthHandler(appengine_config.TWITTER_APP_KEY,
@@ -98,6 +112,7 @@ class TwitterAuth(models.BaseAuth):
     # make sure token key and secret aren't unicode because python's hmac
     # module (used by tweepy/oauth.py) expects strings.
     # http://stackoverflow.com/questions/11396789
+    # fixed in https://github.com/tweepy/tweepy/commit/5a22bf73ccf7fae3d2b10314ce7f8eef067fee7a
     auth.set_access_token(str(token_key), str(token_secret))
     return auth
 
