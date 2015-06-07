17ced05ae53f89479f50ae56abc0705a87721cb0
Ryan Barrett <git@ryanb.org>
2015-06-07 15:34:03 -0700

    drop the dropbox client lib and handle the dropbox oauth 2.0 flow manually

--- oauth_dropins/dropbox.py
+++ oauth_dropins/dropbox.py
@@ -1,24 +1,42 @@
 """Dropbox OAuth drop-in.
 
-Dropbox API docs:
-https://www.dropbox.com/developers/core/start/python
-https://www.dropbox.com/static/developers/dropbox-python-sdk-1.6-docs/
+Standard OAuth 2.0 flow. Docs:
 https://www.dropbox.com/developers/core/docs
+https://www.dropbox.com/developers/reference/oauthguide
 """
 
+import json
 import logging
+import urllib
+import urllib2
 
 import appengine_config
+from appengine_config import HTTP_TIMEOUT
 
-from python_dropbox.client import DropboxOAuth2Flow, DropboxClient
 from google.appengine.ext import ndb
 from webob import exc
 
 import handlers
 import models
-from webutil import handlers as webutil_handlers
 
-CSRF_PARAM = 'dropbox-auth-csrf-token'
+
+# the str() is since WSGI middleware chokes on unicode redirect URLs :/
+GET_AUTH_CODE_URL = str('&'.join((
+  'https://www.dropbox.com/1/oauth2/authorize?'
+  'response_type=code',
+  'client_id=%(client_id)s',
+  'redirect_uri=%(redirect_uri)s',
+  'state=%(state)s',
+)))
+
+GET_ACCESS_TOKEN_URL = str('&'.join((
+  'https://api.dropbox.com/1/oauth2/token?',
+  'grant_type=authorization_code',
+  'code=%(code)s',
+  'client_id=%(client_id)s',
+  'client_secret=%(client_secret)s',
+  'redirect_uri=%(redirect_uri)s',
+)))
 
 
 class DropboxAuth(models.BaseAuth):
@@ -28,8 +46,7 @@ class DropboxAuth(models.BaseAuth):
   OAuth-signed requests to Dropbox's HTTP-based APIs. Stores OAuth credentials
   in the datastore. See models.BaseAuth for usage details.
 
-  Dropbox-specific details: implements urlopen() and api() but not http(). api()
-  returns a python_dropbox.DropboxClient. The key name is the Dropbox user id.
+  Implements urlopen() but not http() or api().
   """
   access_token_str = ndb.StringProperty(required=True)
 
@@ -52,13 +69,6 @@ class DropboxAuth(models.BaseAuth):
     return models.BaseAuth.urlopen_access_token(url, self.access_token_str,
                                                 **kwargs)
 
-  def api(self):
-    """Returns a python_dropbox.DropboxClient.
-
-    Details: https://www.dropbox.com/static/developers/dropbox-python-sdk-1.6-docs/
-    """
-    return DropboxClient(self.access_token_str)
-
 
 class DropboxCsrf(ndb.Model):
   """Stores a CSRF token for the Dropbox OAuth2 flow."""
@@ -66,76 +76,70 @@ class DropboxCsrf(ndb.Model):
   state = ndb.TextProperty(required=False)
 
 
-def handle_exception(self, e, debug):
-  """Exception handler that handles Dropbox client errors.
-  """
-  if isinstance(e, DropboxOAuth2Flow.CsrfException):
-    logging.exception('Bad CSRF token', e)
-    raise exc.HTTPForbidden()
-  elif isinstance(e, (DropboxOAuth2Flow.BadRequestException,
-                      DropboxOAuth2Flow.BadStateException,
-                      DropboxOAuth2Flow.ProviderException)):
-    logging.exception('OAuth error', e)
-    raise exc.HTTPBadRequest()
-  else:
-    return webutil_handlers.handle_exception(self, e, debug)
-
-
 class StartHandler(handlers.StartHandler):
   """Starts Dropbox auth. Requests an auth code and expects a redirect back.
   """
-  handle_exception = handle_exception
-
   def redirect_url(self, state=None):
     assert (appengine_config.DROPBOX_APP_KEY and
             appengine_config.DROPBOX_APP_SECRET), (
       "Please fill in the dropbox_app_key and dropbox_app_secret files in "
       "your app's root directory.")
 
-    csrf = DropboxCsrf(state=state)
-    csrf.put()
-    csrf_holder = {}
-    flow = DropboxOAuth2Flow(appengine_config.DROPBOX_APP_KEY,
-                             appengine_config.DROPBOX_APP_SECRET,
-                             self.request.host_url + self.to_path,
-                             csrf_holder, CSRF_PARAM)
-
-    auth_url = flow.start(url_state=str(csrf.key.id()))
-    csrf.token = csrf_holder[CSRF_PARAM]
-    csrf.put()
-    logging.info('Stored DropboxCsrf id %d', csrf.key.id())
-    return auth_url
+    csrf_key = DropboxCsrf(state=state).put()
+    return GET_AUTH_CODE_URL % {
+      'client_id': appengine_config.DROPBOX_APP_KEY,
+      'redirect_uri': urllib.quote_plus(self.to_url(state=state)),
+      'state': '%s|%s' % (state, csrf_key.id()),
+    }
 
 
 class CallbackHandler(handlers.CallbackHandler):
   """The auth callback. Fetches an access token, stores it, and redirects home.
   """
-  handle_exception = handle_exception
-
   def get(self):
+    # handle errors
+    error = self.request.get('error')
+    error_reason = urllib.unquote_plus(self.request.get('error_reason', ''))
+
+    if error or error_reason:
+      if error == 'access_denied':
+        logging.info('User declined: %s', error_reason)
+        self.finish(None, state=self.request.get('state'))
+        return
+      else:
+        raise exc.HTTPBadRequest(' '.join((error, error_reason)))
+
     # lookup the CSRF token
-    csrf_id = self.request.get('state').split('|')[1]
+    csrf_id = urllib.unquote_plus(self.request.get('state')).split('|')[-1]
     csrf = DropboxCsrf.get_by_id(int(csrf_id))
     if not csrf:
       raise exc.HTTPBadRequest('No CSRF token for id %s', csrf_id)
 
-    # extract the OAuth access token
-    csrf_holder = {CSRF_PARAM: csrf.token}
-    flow = DropboxOAuth2Flow(appengine_config.DROPBOX_APP_KEY,
-                             appengine_config.DROPBOX_APP_SECRET,
-                             self.request.path_url,
-                             csrf_holder, CSRF_PARAM)
+    # request an access token
+    auth_code = self.request.get('code')
+    assert auth_code
+    data = {
+      'client_id': appengine_config.DROPBOX_APP_KEY,
+      'client_secret': appengine_config.DROPBOX_APP_SECRET,
+      'code': auth_code,
+      'redirect_uri': self.request.path_url,
+    }
+
+    logging.debug('Fetching: %s with data %s', GET_ACCESS_TOKEN_URL, data)
     try:
-      access_token, user_id, state = flow.finish(self.request.params)
-    except DropboxOAuth2Flow.NotApprovedException, e:
-      logging.info('User declined OAuth request: %s', e)
-      self.finish(None, state=csrf.state)
-      return
+      resp = urllib2.urlopen(GET_ACCESS_TOKEN_URL, data=urllib.urlencode(data),
+                             timeout=HTTP_TIMEOUT).read()
     except BaseException, e:
       handlers.interpret_http_exception(e)
       raise
 
-    logging.info('Storing new Dropbox account: %s', user_id)
-    auth = DropboxAuth(id=user_id, access_token_str=access_token)
+    try:
+      data = json.loads(resp)
+    except (ValueError, TypeError):
+      logging.exception('Bad response:\n%s', resp)
+      raise exc.HttpBadRequest('Bad Dropbox response to access token request')
+
+    logging.info('Storing new Dropbox account: %s', data['uid'])
+    auth = DropboxAuth(id=data['uid'], access_token_str=data['access_token'])
     auth.put()
     self.finish(auth, state=csrf.state)
