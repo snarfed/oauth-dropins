7adc80917d6db8cb22ff912c6b4d44b25948e372
Ryan Barrett <git@ryanb.org>
2015-06-25 07:26:56 -0700

    use app engine's mox; automatically show logging when running single test(s)

--- oauth_dropins/test/__init__.py
+++ oauth_dropins/test/__init__.py
@@ -1,14 +1,19 @@
-# Add the App Engine SDK's bundled libraries (django, mox, webob, yaml, etc.) to
+# Add the App Engine SDK's bundled libraries (django, webob, yaml, etc.) to
 # sys.path so we can use them instead of adding them all to tests_require in
 # setup.py.
 # https://cloud.google.com/appengine/docs/python/tools/localunittesting#Python_Setting_up_a_testing_framework
 import dev_appserver
 dev_appserver.fix_sys_path()
 
-# Suppress logging by default.
-import logging, sys
-if '--debug' in sys.argv:
-  sys.argv.remove('--debug')
+# Also use the App Engine SDK's mox because it has bug fixes that aren't in pypi
+# 0.5.3. (Annoyingly, they both say they're version 0.5.3.)
+import os, sys
+sys.path.append(os.path.join(dev_appserver._DIR_PATH, 'lib', 'mox'))
+
+# Show logging when running individual test modules, methods, etc; suppress it
+# when running all tests.
+import logging
+if sys.argv[-1] != 'discover':
   logging.getLogger().setLevel(logging.DEBUG)
 else:
   logging.disable(logging.CRITICAL + 1)
