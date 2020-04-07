26c927d2a6612c251518d802e57eef5c929e20a7
stedn <will.stedden@gmail.com>
2020-04-06 18:38:44 -0700

    add user_json, better error handling, url state encoding

--- oauth_dropins/reddit.py
+++ oauth_dropins/reddit.py
@@ -1,5 +1,8 @@
 """reddit OAuth drop-in.
 
+reddit API docs:
+https://github.com/reddit-archive/reddit/wiki/API
+https://www.reddit.com/dev/apis
 praw API docs:
 https://praw.readthedocs.io/en/v3.6.0/pages/oauth.html
 """
@@ -32,6 +35,7 @@ class RedditAuth(models.BaseAuth):
   """
   # access token
   refresh_token = ndb.StringProperty(required=True)
+  user_json = ndb.TextProperty()
   
   def site_name(self):
     return 'reddit'
@@ -54,6 +58,7 @@ class StartHandler(handlers.StartHandler):
   LABEL = 'Reddit'
 
   def redirect_url(self, state=None):
+    # if state is None the reddit API redirect breaks, set to random string
     if not state:
       state = str(randint(100000,999999))
     assert REDDIT_APP_KEY and REDDIT_APP_SECRET, \
@@ -67,7 +72,8 @@ class StartHandler(handlers.StartHandler):
     models.OAuthRequestToken(id=state,
                              token_secret=state,
                              state=state).put()
-    return reddit.auth.url(['identity'], state+':'+self.to_path, 'permanent')
+    st = util.encode_oauth_state({'state':state,'to_path':self.to_path})
+    return reddit.auth.url(['identity'], st, 'permanent')
 
   @classmethod
   def button_html(cls, *args, **kwargs):
@@ -83,15 +89,20 @@ class CallbackHandler(handlers.CallbackHandler):
 
   def get(self):
     error = self.request.get('error')
-    st = self.request.get('state')
-    state = st.split(':')[0]
-    to_path = st.split(':')[1]
+    st = util.decode_oauth_state(self.request.get('state'))
+    state = st.get('state')
+    to_path = st.get('to_path')
     code = self.request.get('code')
     if error or not state or not code:
-      # user declined
-      self.finish(None)
-      return
-    
+      if error in ('access_denied'): 
+        logging.info('User declined: %s', self.request.get('error_description')) 
+        self.finish(None, state=state) 
+        return 
+      else: 
+        msg = 'Error: %s' % (error) 
+        logging.info(msg) 
+        raise exc.HTTPBadRequest(msg) 
+      
     # look up the stored state to check authenticity
     request_token = models.OAuthRequestToken.get_by_id(state)
     if request_token is None:
@@ -103,9 +114,22 @@ class CallbackHandler(handlers.CallbackHandler):
                          user_agent='oauth-dropin reddit identity checker')
 
     refresh_token = reddit.auth.authorize(code)
-    user_id = str(reddit.user.me())
+    user = reddit.user.me()
+    # a short list of attributes to grab 
+    # looks like calling json_dumps on the object opens some kind of stream
+    # https://github.com/praw-dev/praw/blob/master/praw/models/reddit/redditor.py#L41
+    attribute_list = ['name',
+                      'comment_karma',
+                      'created_utc',
+                      'id',
+                      'name',
+                      'link_karma',
+                      'icon_img']
+    user_json = {a:getattr(user,a) for a in attribute_list}
+    user_id = str(user)
     
     auth = RedditAuth(id=user_id,
-                      refresh_token=refresh_token)
+                      refresh_token=refresh_token,
+                      user_json=json_dumps(user_json))
     auth.put()
     self.finish(auth, state=st)
