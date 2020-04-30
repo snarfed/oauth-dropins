b3d3d4b8683237c51e05b7e33b2f96a205fd955b
stedn <will.stedden@gmail.com>
2020-04-30 13:21:10 -0700

    check user is_suspended instead

--- oauth_dropins/reddit.py
+++ oauth_dropins/reddit.py
@@ -135,7 +135,8 @@ def praw_to_user(user):
     user: :class:`praw.models.Redditor`
 
   Note 1: accessing redditor attributes lazily calls reddit API
-  Note 2: subreddit (optional) refers to a user profile (stored as a subreddit)
+  Note 2: if user.is_suspended is True, other attributes will not exist
+  Note 3: subreddit refers to a user profile (stored as a subreddit)
 
   Returns: dict
 
@@ -143,11 +144,8 @@ def praw_to_user(user):
     :class:`prawcore.exceptions.NotFound` if the user doesn't exist or has been
     deleted
   """
-  subr = None
-  try:
-    subr = user.subreddit
-  except AttributeError:
-    logging.info(f'Redditor {user.name} subreddit does not exist')
+  if user.is_suspended:
+    return {}
   return {
     'name': user.name,
     'subreddit': subr,
