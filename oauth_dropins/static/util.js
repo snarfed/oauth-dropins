b6dd14cc14e9d29a46ac1156963dfc620e598ab8
Ryan Barrett <git@ryanb.org>
2021-01-04 14:23:05 -0800

    util.js: add comment that it shouldn't be loaded async

--- oauth_dropins/static/util.js
+++ oauth_dropins/static/util.js
@@ -1,6 +1,10 @@
 /* Add http:// to URL fields on blur or when enter is pressed.
  * From https://aaronparecki.com/2018/06/03/4/url-form-field
  * Thanks Aaron!
+ *
+ * Warning: this script must be loaded synchronously, ie *not* with <script
+ * async>. It adds an event listener on DOMContentLoaded, which some browsers
+ * (eg Chrome) may fire before async scripts are loaded.
  */
 document.addEventListener('DOMContentLoaded', function() {
   function addDefaultScheme(target) {
