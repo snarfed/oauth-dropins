29ad6a31a936b2cf1b59b6e4f061af640d3e554b
Ryan Barrett <git@ryanb.org>
2023-09-20 13:00:09 -0700

    docs: minor config tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -56,11 +56,12 @@ autodoc_default_options = {
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 # napoleon_include_private_with_doc = False
-# napoleon_include_special_with_doc = True
+napoleon_include_special_with_doc = False
 # napoleon_use_admonition_for_examples = False
 # napoleon_use_admonition_for_notes = False
 # napoleon_use_admonition_for_references = False
 # napoleon_use_ivar = False
+# napoleon_use_keyword = True
 # napoleon_use_param = True
 # napoleon_use_rtype = True
 
@@ -336,6 +337,8 @@ texinfo_documents = [
 intersphinx_mapping = {
   'flask': ('https://flask.palletsprojects.com/en/latest', None),
   'flask_caching': ('https://flask-caching.readthedocs.io/en/latest', None),
+  'granary': ('https://granary.readthedocs.io/en/latest', None),
+  'oauth2client': ('https://oauth2client.readthedocs.io/en/latest/', None),
   'python': ('https://docs.python.org/3/', None),
   'requests': ('https://requests.readthedocs.io/en/stable/', None),
   'requests_oauthlib': ('https://requests-oauthlib.readthedocs.io/en/latest/', None),
